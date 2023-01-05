3844f8253cb261f96d97dc1811cf3730cabc7ce2
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-01-05 15:42:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,26 @@ function take_snapshot(){
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPose);
 }
 
 function draw(){
+   image(video, 0, 0, 300, 300);
+}
 
+function modelLoaded(){
+    console.log("PoseNet is Loaded");
+}
+
+function gotPose(results){
+    if(results.lenght > 0){
+        console.log(results);
+        console.log("nose x="+ results[0].pose.nose.x);
+        console.log("nose y="+ results[0].pose.nose.y);
+    }
 }
